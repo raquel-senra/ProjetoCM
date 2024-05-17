8298bceb13e7e885c89a44e67f098846bd278389
raquel-senra <raquelsenra@ipvc.pt>
2024-05-17 23:00:55 +0100

    17.05.2024

---
+++ app/src/main/java/intro/android/projetocm/ui/login/LoginFragment.kt
@@ -0,0 +1,26 @@
+package intro.android.projetocm.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import intro.android.projetocm.R
+import intro.android.projetocm.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root
+
+    }
+}
