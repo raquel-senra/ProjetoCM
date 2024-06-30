bedb33fdf8c5639fa70f46f97e1e7208df9d3fa7
raquel-senra <raquelsenra@ipvc.pt>
2024-06-30 21:12:03 +0100

    30.06.2024

--- app/src/main/java/intro/android/projetocm/ui/tarefa/TarefaFragment.kt
+++ app/src/main/java/intro/android/projetocm/ui/tarefa/TarefaFragment.kt
@@ -5,15 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.google.firebase.auth.FirebaseAuth
+import intro.android.projetocm.databinding.FragmentTarefaBinding
 
 
 class TarefaFragment : Fragment() {
+     private lateinit var navController: NavController
+     private lateinit var binding: FragmentTarefaBinding
+     private lateinit var auth: FirebaseAuth
 
 
 
+     override fun onCreateView(
+          inflater: LayoutInflater,
+          container: ViewGroup?,
+          savedInstanceState: Bundle?
+     ): View {
+          binding = FragmentTarefaBinding.inflate(inflater, container, false)
+          val root: View = binding.root
+          return root
+     }
 
-     fun onCreateView(){}
+     private fun init(view: View) {
+          navController = Navigation.findNavController(view)
+          auth = FirebaseAuth.getInstance()
+     }
 
 
 }
